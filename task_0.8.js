ab956e568609d4290a24a4eece8f496685db2f4f
Lindokuhle Momoza <lindokuhle.momoza@gmail.com>
2021-01-25 18:56:54 +0200

    addressed requested changes on the time converter

--- task_0.8.js
+++ task_0.8.js
@@ -1,12 +1,16 @@
 function timeConvert(num) {
-  var hours = Math.floor(num / 60)
-  var minutes = num % 60
+  var hours = Math.floor(num / 60);
+  var minutes = num % 60;
 
-  if (hours < 2) {
-    return hours + " hour" + ", " + minutes + " minutes"
+  if (hours < 2 && minutes < 2) {
+    return hours + " hour" + ", " + minutes + " minute";
+  } else if (hours < 2) {
+    return hours + " hour" + ", " + minutes + " minutes";
+  } else if (minutes < 2) {
+    return hours + " hours" + ", " + minutes + " minute";
   } else {
-    return hours + " hours" + ", " + minutes + " minutes"
+    return hours + " hours" + ", " + minutes + " minutes";
   }
 }
 
-console.log(timeConvert(133));
+console.log(timeConvert(72));
